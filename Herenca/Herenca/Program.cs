d7bfb81df90c751bc4df3570e963d91504801172
orsolon <56239735+orsolon@users.noreply.github.com>
2021-08-26 22:11:54 -0300

    projeto

---
+++ Herenca/Herenca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta1 = new ContaCorrente(20);
+            var conta2 = new ContaInvestimento(30);
+            var conta3 = new ContaPoupanca(14);
+
+            var contaDestino = ConverteConta<ContaCorrente, ContaPoupanca>(conta1);
+            
+        }
+
+        public static G ConverteConta<T, G>(T contaOrigem)
+            where T : conta
+            where G : conta
+        {
+            G contaDestino = Activator.CreateInstance<G>();
+            return contaDestino;
+
+        }
+    }
+}
